9e54b75cbd777db6dc13cb5561cedae157b3e1b8
sergik123 <phoenix925012@yandex.ru>
2017-03-01 13:57:32 +0200

    Update IndexController.php

--- www/app/controllers/IndexController.php
+++ www/app/controllers/IndexController.php
@@ -8,13 +8,7 @@
 		private $view;
 		private $addmodel;
 		public function mainAction(){
-			//$this->view=new ViewController;
 			$this->generateviewAction('index');
-
-			if(!empty($_POST['sub'])){
-				$this->addmodel=new AddUser;
-				$this->addmodel->add($_POST);
-			}
 		}
 	}
  ?>
